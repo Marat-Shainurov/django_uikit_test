060b72c6bb272a3dd692ec6585bcd55eedf2b4e7
Marat-Shainurov <m_shainurov@mail.ru>
2023-05-20 13:00:03 +0300

    view and template have been updated

--- main/views.py
+++ main/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 
 
+def calc_func(form_input, request):
+    res = None
+    if form_input != '':
+        is_round = request.POST.get('round')
+        try:
+            if is_round == 'yes':
+                res = round(eval(form_input))
+            else:
+                res = eval(form_input)
+        except ZeroDivisionError:
+            res = ZeroDivisionError.__doc__
+        except Exception:
+            res = 'Incorrect input'
+    return res
+
+
 def index(request):
-    return render(request, 'main/index.html')
+    equation_res = ''
+    if request.method == 'POST':
+        equation = request.POST.get('equation')
+        equation_res = calc_func(equation, request)
+    context = {'equation_res': equation_res}
+    return render(request, 'main/index.html', context)
